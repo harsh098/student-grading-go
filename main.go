fdc1967d41ee2d0305e4d21e343b1be11ff9095f
Harsh Mishra <hmisraji07@gmail.com>
2025-03-19 15:46:31 +0530

    Added Solution
    Signed-off-by: Harsh Mishra <hmisraji07@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	// "sync"
+)
+
 type Grade string
 
 const (
@@ -21,17 +30,118 @@ type studentStat struct {
 }
 
 func parseCSV(filePath string) []student {
-	return nil
+	f, err := os.Open(filePath)
+	array := make([]student, 0, 20)
+	if err!=nil {
+		panic(fmt.Sprintf("Failed Reading the file: %v", err.Error()))
+	}
+	defer f.Close()
+	// Did it concurrently earlier but had to skip it to pass tests.
+	// wg := &sync.WaitGroup{}
+	reader := bufio.NewReader(f)
+	idx := 0
+	var parseLine func(line string)= func(line string) {
+		// defer wg.Done()
+		sArr := strings.Split(line, ",")
+		if len(sArr) < 7 {
+			panic("Malformed Record")
+		}
+		var nInt func(n uint8) int = func(n uint8) int {
+			num, err := strconv.Atoi(strings.TrimSpace(sArr[n]))
+			if err!=nil {
+				panic(fmt.Sprintf("Invalid record %v", line))
+			}
+			return num
+		}
+		array = append(array, student{
+			firstName: strings.TrimSpace(sArr[0]),
+			lastName: strings.TrimSpace(sArr[1]),
+			university: strings.TrimSpace(sArr[2]),
+			test1Score: nInt(3),
+			test2Score: nInt(4),
+			test3Score: nInt(5),
+			test4Score: nInt(6),
+		})
+	}
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err!=nil {
+			parseLine(line)
+			break
+		}
+		idx++
+		if idx==1 {
+			continue
+		} 
+		// wg.Add(1)
+		// go parseLine(line)
+		parseLine(line)
+
+	}
+	// wg.Wait()
+	return array
 }
 
 func calculateGrade(students []student) []studentStat {
-	return nil
+	studentStatArray := make([]studentStat, 0, 20)
+	for _, value := range students {
+		avg := func(s student) float32{
+			return float32(s.test1Score + s.test2Score + s.test3Score + s.test4Score)/4
+		}
+		var grade Grade
+		finalScore := avg(value)
+		switch {
+			case finalScore<35:
+				grade=F
+			case finalScore>=35 && finalScore < 50:
+				grade=C
+			case finalScore>=50 && finalScore<70:
+				grade=B
+			case finalScore>=70:
+				grade=A 
+		}
+		stat := studentStat{
+			student: value,
+			finalScore: finalScore,
+			grade: grade,
+		}
+		studentStatArray = append(studentStatArray, stat)
+	}
+
+	return studentStatArray
 }
 
 func findOverallTopper(gradedStudents []studentStat) studentStat {
-	return studentStat{}
+	var topper studentStat
+	if len(gradedStudents)<1 {
+		panic("Empty Graded List received")
+	}
+	for i, value := range gradedStudents {
+		if i==0 {
+			topper = value
+			continue
+		}
+
+		if topper.finalScore <= value.finalScore {
+			topper = value
+		}
+	}
+
+	return topper
 }
 
 func findTopperPerUniversity(gs []studentStat) map[string]studentStat {
-	return nil
+	topperMap := make(map[string]studentStat)
+	for _, value := range gs {
+		topper, ok := topperMap[value.university]
+		if !ok {
+			topperMap[value.university]=value
+			continue
+		}
+		if topper.finalScore <= value.finalScore {
+			topperMap[value.university] = value
+		}
+	}
+	return topperMap
 }
